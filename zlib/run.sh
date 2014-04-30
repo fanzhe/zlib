3727f92841b8270ddcbed21ba400524626184cbb
fanzhe <fanzhe.hkbu@gmail.com>
2014-04-30 21:06:36 +0800

    great

--- zlib/run.sh
+++ zlib/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-./zlib.exe r data/yeast/query/cliques/yeast_clique2.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_clique2.detailed data/yeast/result/yeast_clique2.avg 
-./zlib.exe r data/yeast/query/cliques/yeast_clique3.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_clique3.detailed data/yeast/result/yeast_clique3.avg
-./zlib.exe r data/yeast/query/cliques/yeast_clique4.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_clique4.detailed data/yeast/result/yeast_clique4.avg
-./zlib.exe r data/yeast/query/cliques/yeast_clique5.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_clique5.detailed data/yeast/result/yeast_clique5.avg
+./zlib.exe r data/yeast/query/cliques/yeast_clique6.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_clique6.detailed data/yeast/result/yeast_clique6.avg
 
