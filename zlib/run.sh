0f3e0e23a6d57b782950923d348c334416d021d0
fanzhe <fanzhe.hkbu@gmail.com>
2014-05-01 10:02:05 +0800

    great

--- zlib/run.sh
+++ zlib/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-./zlib.exe r data/yeast/query/subgraph/yeast_q1.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q1.detailed data/yeast/result/yeast_q1.avg
-./zlib.exe r data/yeast/query/subgraph/yeast_q2.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q2.detailed data/yeast/result/yeast_q2.avg
-./zlib.exe r data/yeast/query/subgraph/yeast_q3.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q3.detailed data/yeast/result/yeast_q3.avg
-./zlib.exe r data/yeast/query/subgraph/yeast_q4.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q4.detailed data/yeast/result/yeast_q4.avg
-./zlib.exe r data/yeast/query/subgraph/yeast_q5.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q5.detailed data/yeast/result/yeast_q5.avg
-./zlib.exe r data/yeast/query/subgraph/yeast_q6.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q6.detailed data/yeast/result/yeast_q6.avg
+./zlib.exe r data/yeast/query/subgraphs/yeast_q1.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q1.detailed data/yeast/result/yeast_q1.avg
+./zlib.exe r data/yeast/query/subgraphs/yeast_q2.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q2.detailed data/yeast/result/yeast_q2.avg
+./zlib.exe r data/yeast/query/subgraphs/yeast_q3.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q3.detailed data/yeast/result/yeast_q3.avg
+./zlib.exe r data/yeast/query/subgraphs/yeast_q4.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q4.detailed data/yeast/result/yeast_q4.avg
+./zlib.exe r data/yeast/query/subgraphs/yeast_q5.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q5.detailed data/yeast/result/yeast_q5.avg
+./zlib.exe r data/yeast/query/subgraphs/yeast_q6.igraph 1000 data/yeast/yeast.my 1 data/yeast/result/yeast_q6.detailed data/yeast/result/yeast_q6.avg
